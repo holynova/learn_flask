15b8ce0f28260ac285373e25c373f2613299092a
moonreader <holy_nova@126.com>
2016-06-02 10:55:34 +0800

    搭建一个多页面的完整网站（没有数据库）

--- learn_flask.py
+++ learn_flask.py
@@ -1,6 +1,8 @@
 # -*- coding: cp936 -*-
-from flask import Flask,url_for
+from flask import Flask,url_for,render_template
 import datetime
+import json
+import random
 
 
 app = Flask(__name__)
@@ -16,24 +18,62 @@ link = '''
 </ul>
 <hr>
 '''
+# cnt = 0
+class Article(object):
+	def __init__(self,id,title="default title",author="default author",content='hello world'):
+		self.id = id
+		self.title = title
+		self.author = author
+		self.content = content
+		self.gen_random()
+	def gen_random(self):
+		self.title = random.choice(['News','Joke','Info'])+str(random.choice(range(999)))
+		self.author =random.choice(['Stave Jobs','Bill Gats','Adam Sang'])
+		self.content = 1*('test content: \nid = %s\n title = %s\n author = %s\n' %(self.id,self.title,self.author))
 
+	
+art_arr = []
+for i in range(50):
+	art_arr.append(Article(id = i))
+art_dic = {}
+for i in range(50):
+	art = Article(id = i)
+	art_dic[art.id] = art
 
 @app.route('/')
+@app.route('/main')
 def go():
+	# return render_template()
 	str_now = datetime.datetime.now().strftime('%y%m%d %H:%M:%S')
 	url =  url_for('static',filename = 'main.css')
-	return link+'<h1>datetime</h1>'+str_now+' static_url='+url
+	# return link+'<h1>datetime</h1>'+str_now+' static_url='+url
+	return render_template('base.html')
 # def head():
 # 	return 'Hello flask'
+
 @app.route('/404')
 def show404():
 	return link+'<h1>404 not found</h1>'
+
 @app.route('/about')
 def show_about():
-	return link+'about page'
-# @app.route('/<page_num>')
-# def show_page(page_num):
-# 	return link+'page num is %s' %(page_num)
+	# return link+'about page'
+	return render_template('about.html')
+
+@app.route('/list')
+
+
+def show_list():
+	return render_template('list.html',art_dic = art_dic)
+
+
+@app.route('/article/<id>')
+def show_article(id):
+	# return '<h1>'+title+'</h1>'
+	return render_template("article.html",a =art_dic[int(id)])
+
+# @app.route(url_for(static),filename='main.css')
+# pass
 
 if __name__ == '__main__':
 	app.run(debug= True)
---
+++ static/main.js
@@ -0,0 +1,6 @@
+// window.onload(init);
+window.addEventListener('load',init,false);
+
+function init(){
+	console.log('main.js is ready to work');
+}
